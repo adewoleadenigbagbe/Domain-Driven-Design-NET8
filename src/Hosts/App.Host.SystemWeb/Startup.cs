69e4f2a53a1e68d184968cfb41099133b36723ea
Adewole <wolecharles@gmail.com>
2024-10-24 11:24:39 +0100

    add newtonsoft packages

--- src/Hosts/App.Host.SystemWeb/Startup.cs
+++ src/Hosts/App.Host.SystemWeb/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using App.Api.Controllers;
+using App.Api.Converters;
 using App.Api.Filters;
 using App.Host.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel;
 
 namespace App.Host.SystemWeb
 {
@@ -32,11 +36,19 @@ namespace App.Host.SystemWeb
             services.AddControllers(opt =>
             {
                 opt.Filters.Add<ValidateModel>();
-            }).AddApplicationPart(typeof(ProductController).Assembly);
+            })
+            .AddNewtonsoftJson(opt =>
+            {
+                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                opt.SerializerSettings.Converters.Add(new TrimConverter());
+            })
+            .AddApplicationPart(typeof(ProductController).Assembly);
 
             services.AddEndpointsApiExplorer();
 
             services.AddSwaggerGen();
+
+            services.AddCors();
 
             ServiceRegistration.Register(services);
         }
---
+++ src/Services/App.Api/Converters/TrimConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Api.Converters
+{
+    public class TrimConverter : JsonConverter
+    {
+        public override bool CanRead => true;
+
+        public override bool CanWrite => false;
+
+        public override bool CanConvert(Type objectType) =>  objectType == typeof(string);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return ((string)reader.Value)?.Trim();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
